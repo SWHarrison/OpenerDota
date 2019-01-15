f79e0212eb14e63a635e73309f25edffbd52dc64
Sam Harrison <samuel.harrison@students.makeschool.com>
2019-01-14 21:23:45 -0800

    user testing live

--- controllers/routes.js
+++ controllers/routes.js
@@ -23,7 +23,7 @@ module.exports = app => {
     app.post("/sign-up", (req, res) => {
     // Create User and JWT
         const user = new User(req.body);
-
+        console.log(process.env.SECRET)
         user.save().then((user) => {
             var token = jwt.sign({ _id: user._id }, process.env.SECRET, { expiresIn: "60 days" });
             res.cookie('nToken', token, { maxAge: 900000, httpOnly: true });
